df5eb3db6b0c4ab460d477a76f1d7f3f5401038f
张丹江 <danjiang.zdj@alibaba-inc.com>
2017-06-16 10:55:03 +0800

    【反复】

--- app/src/main/java/com/jing/demo01/MainActivity.java
+++ app/src/main/java/com/jing/demo01/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //测试提交数据
+
+        // 在测试
     }
 }
